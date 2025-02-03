d3689a1a781d777a4e82b75d8b48ba42ae4143f9
Janaki Ram Kadiyala <jani555k@gmail.com>
2025-02-03 21:55:21 +0530

    Master Branch Creation

---
+++ src/test/java/com/jr/backedndproject_feb2025/BackedndProjectFeb2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.backedndproject_feb2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackedndProjectFeb2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
